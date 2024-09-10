d50edd08e98d4cda5fd8f075b9f60bc08fac07ef
Maximiliano Osorio <maxiosorio@gmail.com>
2024-09-10 10:15:35 -0300

    add: calculate profit using example data

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import BasicDateCalendar from './components/BasicDateCalendar';
 import dayjs, { Dayjs } from 'dayjs';
+import { Portfolio, Stock } from './models/portafolio';
 
 function App() {
   const [startDate, setStartDate] = useState<Dayjs>(dayjs('2023-01-03'));
   const [endDate, setEndDate] = useState<Dayjs>(dayjs('2024-08-30'));
+
+  const [profit, setProfit] = useState<number>(0);
+
+  useEffect(() => {
+    const portfolio = new Portfolio();
+    portfolio.addStock(new Stock('AAPL', 10));
+    portfolio.addStock(new Stock('MSFT', 20));
+    try {
+      const profit = portfolio.profit(startDate.toDate(), endDate.toDate());
+      setProfit(profit);
+    } catch (e) {
+      console.error(e.message());
+    }
+  }, [startDate, endDate]);
 
   return (
     <>
@@ -21,6 +36,7 @@ function App() {
         setValue={setEndDate}
         minDate={startDate}
       />
+      <span>{profit}</span>
     </>
   );
 }
